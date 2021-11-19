19db408ef838768441038ee03701475febbccce7
LuLourengo <85420018+LuLourengo@users.noreply.github.com>
2021-11-18 22:00:19 -0300

    Add files via upload

---
+++ php/index.php
@@ -0,0 +1,12 @@
+<?php
+
+echo "<h4>Lucia Lourengo Caridade<h4>";
+echo "Edad: 22 Años";
+echo "</br>";
+echo "Hobbies: Jugar al Basket, sacar fotos";
+echo "</br>";
+echo "Editor de codigo preferido: Sublime Text";
+echo "</br>";
+echo "Sistema operativo:".PHP_OS ;
+
+?>
